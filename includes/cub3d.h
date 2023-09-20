2445d229c9a8342e0966a7e903d1c63a7c90c87d
Darkb0shy <dcarassi@localhost.localdomain>
2023-09-20 10:51:06 +0200

    header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -19,50 +19,6 @@
 #define WIDTH 1280
 #define TEXTURE_SIZE 64
 
-// MLX keys
-# define K_Q 12
-# define K_W 13
-# define K_E 14
-# define K_R 15
-# define K_T 17
-# define K_Y 16
-# define K_U 32
-# define K_I 34
-# define K_O 31
-# define K_P 35
-# define K_A 0
-# define K_S 1
-# define K_D 2
-# define K_F 3
-# define K_G 5
-# define K_H 4
-# define K_J 38
-# define K_K 40
-# define K_L 37
-# define K_Z 6
-# define K_X 7
-# define K_C 8
-# define K_V 9
-# define K_B 11
-# define K_N 45
-# define K_M 46
-# define K_ESCAPE 53
-# define K_ENTER 36
-# define K_BACKSPACE 51
-# define K_LEFT 123
-# define K_RIGHT 124
-# define K_DOWN 125
-# define K_UP 126
-
-// MLX Linux Keys
-# define LK_W 119
-# define LK_A 97
-# define LK_S 115
-# define LK_D 100
-# define LK_LEFT 65361
-# define LK_RIGHT 65363
-# define LK_ESCAPE 65307
-
 typedef struct s_vec2
 {
 	double	x;
